632e83e41cb70d3624901bed8e397a0cb9c26711
IACarvalho <israelcarvalhodesenvolvedor@gmail.com>
2022-12-22 19:48:49 -0300

    Interpolacao

--- Curso-Angular/primeiro-projeto/src/app/cursos/cursos.component.ts
+++ Curso-Angular/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent {
+  htmlCurso: string;
+  nomeDoPortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular']
 
+  constructor() {
+    this.htmlCurso = 'http://loiane.training';
+    this.nomeDoPortal = 'Loraine Training'
+  }
 }
